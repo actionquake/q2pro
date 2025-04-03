70911f786841aa3fc9f118c0bb8dd91cf423b58b
Dino <8dino2@gmail.com>
2025-04-03 12:37:09 -0400

    Fixed idle sounds played in training mode

--- src/action/p_client.c
+++ src/action/p_client.c
@@ -6503,7 +6503,7 @@ void ClientBeginServerFrame(edict_t * ent)
 
 		if( (ppl_idletime->value > 0) && idleframes && (idleframes % (int)(ppl_idletime->value * HZ) == 0) )
 			//plays a random sound/insane sound, insane1-11.wav
-			if (!jump->value || training->value) // Don't play insane sounds in jmod or training mode
+			if (!jump->value || !training->value) // Don't play insane sounds in jmod or training mode
 				gi.sound( ent, CHAN_VOICE, gi.soundindex(va( "insane/insane%i.wav", rand() % 11 + 1 )), 1, ATTN_NORM, 0 );
 
 		if( (sv_idleremove->value > 0) && (idleframes > (sv_idleremove->value * HZ)) && client->resp.team )
