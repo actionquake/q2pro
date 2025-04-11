8efba3d99f983026a3875d8e620dcd97cb796417
Dino <8dino2@gmail.com>
2025-04-11 12:09:02 -0400

    Added comment in glass

--- src/action/cgf_sfx_glass.c
+++ src/action/cgf_sfx_glass.c
@@ -275,7 +275,7 @@ CGF_SFX_ShootBreakableGlass (edict_t * aGlassPane, edict_t * anAttacker,
       case MOD_KNIFE:		// slash damage
         destruct = true;
         break;
-      default:
+      default:   //Otherwise, randomly break
         destruct = (rand () % 3 == 0);
         break;
       };
