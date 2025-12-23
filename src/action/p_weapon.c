40b9c673f6562d9860ccd0f77c4c83c6e937fbf0
Aaron Dean <8dino2@gmail.com>
2025-12-22 21:33:20 -0500

    More CTF Fixes (#265)
    * CTF fixes, null checks
    
    * More ctf_mode fixes

--- src/action/p_weapon.c
+++ src/action/p_weapon.c
@@ -500,7 +500,7 @@ void ChangeWeapon(edict_t* ent)
 
 	// TODO: Make this work
 	// CTB prevents changing to a non-mk23/knife/grenade if carrying a briefcase
-	if (ctf_mode->value && 
+	if (ctf_mode->value == 2 && 
 	(ent->client->inventory[ITEM_INDEX(team_flag[TEAM1])] ||
 	ent->client->inventory[ITEM_INDEX(team_flag[TEAM2])]))
 	{
