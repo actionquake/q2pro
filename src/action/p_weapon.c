b6fcf8e9aac54fbdac9cc5d7e216823e6a56c8be
Aaron Dean <8dino2@gmail.com>
2025-12-22 23:29:06 -0500

    Restored ctf_mode design, added docs, fixes (#266)
    * Restored ctf_mode design
    
    * If statement fix for CTB

--- src/action/p_weapon.c
+++ src/action/p_weapon.c
@@ -500,7 +500,7 @@ void ChangeWeapon(edict_t* ent)
 
 	// TODO: Make this work
 	// CTB prevents changing to a non-mk23/knife/grenade if carrying a briefcase
-	if (ctf_mode->value == 2 && 
+	if (ctf_mode->value && 
 	(ent->client->inventory[ITEM_INDEX(team_flag[TEAM1])] ||
 	ent->client->inventory[ITEM_INDEX(team_flag[TEAM2])]))
 	{
